641790442888fd0c57420224cd47b6f9c5001a0b
sparksys98 <hanineiroukh@gmail.com>
2020-03-18 17:05:08 +0200

    k

--- src/AuthorsList.js
+++ src/AuthorsList.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { connect } from "react-redux";
 // Components
 import AuthorCard from "./AuthorCard";
 
@@ -15,5 +15,9 @@ const AuthorsList = props => {
     </div>
   );
 };
-
-export default AuthorsList;
+const mapStateToProps = state => {
+  return {
+    authors: state.authors
+  };
+};
+export default connect(mapStateToProps)(AuthorsList);
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import { addAuthor } from "./redux/actionCreators";
 const Sidebar = props => {
   return (
     <div id="sidebar">
@@ -11,7 +12,7 @@ const Sidebar = props => {
         <button
           id="add-button"
           className="btn btn-block btn-light"
-          onClick={props.addAuthorHandler}
+          onClick={props.addAuthor}
         >
           + ADD AUTHOR
         </button>
@@ -19,5 +20,9 @@ const Sidebar = props => {
     </div>
   );
 };
-
-export default Sidebar;
+const mapDispatchToProps = dispatch => {
+  return {
+    addAuthor: () => dispatch(addAuthor())
+  };
+};
+export default connect(null, mapDispatchToProps)(Sidebar);
